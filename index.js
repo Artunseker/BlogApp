db9abe744b9b4a1f237d2e0edb46e97850e2b16d
ArtunSeker <artunseker2005@gmail.com>
2025-03-26 00:07:24 +0300

    middleware

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const app = express();//uygulamamızı oluşturduk
 
+app.use((req,res,next)=>{
+    console.log("middleware 1");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("middleware 2");
+    next();
+});
+
 app.use((req,res)=>{
-    res.end("Ana Sayfa");
+    console.log("middleware 3");
+    res.send("<h1>home page</h1>");
 });
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
-});
+}); 
