39b2c4af68d0093b8aec8903e1339a5056f0ff6c
ArtunSeker <artunseker2005@gmail.com>
2025-03-27 22:30:19 +0300

    Static dosyaların hazırlanması

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();//uygulamamızı oluşturduk
 
+app.use("/libraries",express.static("node_modules"));//takma isim libraries, html gibi static dosyalar için kullanılır
+app.use("/static",express.static("public"));//public klasör dosyalar için kullanılır fakat static adı ile html de kullanılır. Çünkü daha güvenlidir.
+
 app.use("/blogs/:blogid",(req,res)=>{
     console.log(__dirname);
     console.log(__filename);
