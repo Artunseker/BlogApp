6a270132ae2f84bdb7aeb9c55f1b913038b73fb1
ArtunSeker <artunseker2005@gmail.com>
2025-04-02 20:44:19 +0300

    Blog_Detail_Sayfası

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,28 @@ const router = express.Router();
 
 const db=require("../data/db.js"); 
 
-router.use("/blogs/:blogid",(req,res)=>{
-    
-    res.render('users/blog-details');
+router.use("/blogs/:blogid",async function(req,res){
+    const id =req.params.blogid;
+
+    try{
+        const [blog] = await db.execute("SELECT * FROM blog where blogid=?",[id]);
+        if(blog[0]){
+            return res.render('users/blog-details',{
+                title:blog[0].baslik,
+                blog:blog[0],
+            });
+        }
+        res.redirect("/blogs");
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 router.use("/blogs",async function(req,res){
     try{
-        const [blogs, ] = await db.execute("SELECT * FROM blog where onay=1 ");
-        const [categories, ] = await db.execute("SELECT * FROM categories");
+        const [blogs] = await db.execute("SELECT * FROM blog where onay=1 ");//await ile sırasıyla sorgular çalışır ilk blog onay=1 i alır sonra categories i alır
+        const [categories] = await db.execute("SELECT * FROM categories");
         
         res.render('users/blogs',{
             title:"Tüm Kurslar",
@@ -27,8 +40,8 @@ router.use("/blogs",async function(req,res){
 router.use("/", async function(req,res){
 
     try{
-        const [blogs, ] = await db.execute("SELECT * FROM blog where onay=1 and anasayfa=1");
-        const [categories, ] = await db.execute("SELECT * FROM categories");
+        const [blogs] = await db.execute("SELECT * FROM blog where onay=1 and anasayfa=1");
+        const [categories] = await db.execute("SELECT * FROM categories");
 
         res.render('users/index',{
             title:"Popüler Kurslar",
