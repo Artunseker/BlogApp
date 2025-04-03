84a99e9c544699a24726377d05f231999e080fad
ArtunSeker <artunseker2005@gmail.com>
2025-04-03 23:28:19 +0300

    blog ekleme formu

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const adminRouter = express.Router();
 
-adminRouter.use("/blog/create",(req,res)=>{
-    res.render('admin/blog-create');
+const db=require("../data/db");
+
+
+adminRouter.use("/blog/create",async (req,res)=>{
+    try{
+        const [categories,]=await db.execute("Select * from categories");
+
+        res.render('admin/blog-create',{
+            title:"Blog Ekleme",
+            categories:categories
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 adminRouter.use("/blogs/:blog_id",(req,res)=>{
